624ad78aa10a45c15c81cccf9f2fa791f5295c68
ekuzmin <kuzkuzq@yandex.ru>
2023-06-28 12:33:57 +0300

    И еще

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: import.meta.env.PROD === 'production' ? '/smartex/' : '/'
+  base: import.meta.env.MODE === 'production' ? '/smartex/' : '/'
 })
 
 console.log(import.meta)
